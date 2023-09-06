dc96ec21f3b779b015a38aa192ae1e844634ca74
David Way <david_way@live.ie>
2023-09-06 22:16:25 +0100

    fix: missing site metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "!important",
-	url: "https://example.com/",
+	url: "https://david-way.github.io/",
 	language: "en",
 	description: "I am writing about my not important experiences.",
 	author: {
 		name: "David Way",
 		email: "david_way@live.ie",
-		url: "https://example.com/about-me/"
+		url: "https://david-way.github.io/about-me/"
 	}
 }
