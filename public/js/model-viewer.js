2e5fc3bc13ddaa6d16f58ea829c2a70521bcede5
David Way <david_way@live.ie>
2026-03-07 15:20:45 +0000

    fix: three js import

--- public/js/model-viewer.js
+++ public/js/model-viewer.js
@@ -1,5 +1,5 @@
+import * as THREE from 'three';
 import { OrbitControls } from '/js/OrbitControls.js';
-import * as THREE from "/js/three.module.js";
 import { STLLoader } from "/js/STLLoader.js";
 
 document.addEventListener("DOMContentLoaded", () => {
